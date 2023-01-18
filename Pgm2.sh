fe3f2e888f197fde549ff72c74d8618e68cb1a19
RohithMuraleedharan1971 <rohithmuraleedharan007@gmail.com>
2023-01-18 15:23:07 +0530

    Arthmetic Operations

---
+++ Pgm2.sh
@@ -0,0 +1,11 @@
+echo " ENTER TWO NUMBERS : "
+read a
+read b
+s=` expr $a + $b `
+echo " SUM IS : $s "
+m=` expr $a + $b `
+echo " DIFFERENCE IS : $m "
+p=` expr $a + $b `
+echo " PRODUCT IS : $p "
+d=` expr $a + $b `
+echo " QUOTIENT IS : $d "
